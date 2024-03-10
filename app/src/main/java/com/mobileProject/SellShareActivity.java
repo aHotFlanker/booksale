e14963ce2c9b7fe21fd9283d3021f2d6d6b101a2
Dogukan Kahraman <dkahraman08@gmail.com>
2024-03-10 11:16:05 -0700

    Initial commit

---
+++ app/src/main/java/com/mobileProject/SellShareActivity.java
@@ -0,0 +1,14 @@
+package com.mobileProject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell_share);
+    }
+}
